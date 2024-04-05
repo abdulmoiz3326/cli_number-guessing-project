8c1be7ec6021f46fdf23d92e075b9c26e69d1ab3
abdulmoiz3326 <163580355+abdulmoiz3326@users.noreply.github.com>
2024-04-06 03:12:15 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+import inquirer from "inquirer";
+const randomNumber = (2);
+const answers = await inquirer.prompt([
+    {
+        name: "userguessednumber",
+        type: "number",
+        message: "please guess a number: ",
+    },
+]);
+const { userguessednumber } = answers;
+console.log(userguessednumber, "userguessednumber", randomNumber);
+if (userguessednumber === randomNumber) {
+    console.log("Your answer is correct \n You Win ");
+}
+else {
+    console.log("Please Try Again Better Luck Next Time ");
+}
